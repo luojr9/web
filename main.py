5a01ac9cabc43a2aec97d7eebea57be194583b62
Dustin Luo <luojr9@midea.com>
2023-05-29 17:14:22 +0800

    Rename index.py to main.py

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ from bottle import get, post, run, request, template
 
 import os
 
+#### 这个是 客户端请求 服务端就发给一个 index.html 控制界面给客户端
 @get("/")
 def index():
     return template("index")
 
 
-#### 这个是 客户端请求 服务端就发给一个 index.html 控制界面给客户端
 @post("/cmd")
 def cmd():
     adss = request.body.read().decode()  #### 接收到 客户端 发过来的数据
     print("### Do cmd:" + adss)
-    os.system(adss)
+    ret = os.system(adss)
+    print('ret=' + ret)
     return "OK"
 
 
